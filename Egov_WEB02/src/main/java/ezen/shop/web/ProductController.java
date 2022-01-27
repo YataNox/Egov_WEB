3b213826687967bd698efda3e80f3a50fb96c61c
YataNox <rladnwls0523@naver.com>
2022-01-27 21:12:31 +0900

    ,.

--- Egov_WEB02/src/main/java/ezen/shop/web/ProductController.java
+++ Egov_WEB02/src/main/java/ezen/shop/web/ProductController.java
@@ -1,8 +1,11 @@
 package ezen.shop.web;
 
 import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import ezen.shop.service.ProductService;
 
@@ -14,4 +17,8 @@ public class ProductController {
 	// ProductServiceimpl 클래스의 인스턴스가 @Resource에 의해서 자동주입됩니다.
 	// @Autowired는 클래스 이름으로 주동주입할 클래스를 검색하지만, @Resource는 어노테이션 value에 있는 이름으로 검색합니다.
 	
+	@RequestMapping(value="/main.do")
+	public String main(HttpServletRequest request, Model model) {
+		return "main";
+	}
 }
